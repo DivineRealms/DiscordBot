bc90335693411c3ad206718199acef731881a11f
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-11-28 18:36:58 +0100

    Test ban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -11,12 +11,14 @@ module.exports = {
 module.exports.run = async(client, message, args) => {
   if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", `Sorry, I am missing my required permissions perhaps try moving my role up!`)]})
 
-  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+  let isObject = true;
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || args[0]
+  if(!isNaN(args[0])) isObject = false;
 
   if (!args[0]) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "You need to mention user.", "RED")] });
   if (!member) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "Couldn't find that user.", "RED")] });
-  if (member.id === message.author.id) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "You cannot ban yourself.", "RED")] });
-  if (!member.bannable) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "I can't ban that user.", "RED")] });
+  if (isObject == true && member.id === message.author.id) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "You cannot ban yourself.", "RED")] });
+  if (isObject == true && !member.bannable) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "I can't ban that user.", "RED")] });
 
   const reason = args.slice(1).join(" ") || 'No Reason'
 
@@ -24,7 +26,7 @@ module.exports.run = async(client, message, args) => {
 
   client.utils.logs(client, message.guild, "User Warned", [{
     name: "User",
-    desc: member.user
+    desc: isObject == true ? member.user : `<@!${args[0]}>`
   },{
     name: "Staff",
     desc: message.author
@@ -34,7 +36,7 @@ module.exports.run = async(client, message, args) => {
   },{
     name: "Duration",
     desc: "Permanent"
-  }], member.user);
+  }], isObject == true ? member.user : message.author);
 
   message.guild.members.ban(member, { reason })
   await message.channel.send({ embeds: [embed] })
