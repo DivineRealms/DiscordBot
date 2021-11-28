70da9508f1eeec8142fd13abcc42bfaf0ed47760
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-11-28 18:58:27 +0100

    Fix

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -11,7 +11,7 @@ module.exports = {
 module.exports.run = async(client, message, args) => {
   if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", `Sorry, I am missing my required permissions perhaps try moving my role up!`)]})
 
-  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || args[0]
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
   if(member) {
     if (member.id == message.author.id) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "You cannot ban yourself.", "RED")] });
@@ -38,6 +38,7 @@ module.exports.run = async(client, message, args) => {
     message.guild.members.ban(member, { reason })
     await message.channel.send({ embeds: [embed] }) 
   } else if(!isNaN(args[0])) {
+    member = args[0]
     if (args[0] === message.author.id) return message.channel.send({ embeds: [client.embedBuilder(client, message, "Error", "You cannot ban yourself.", "RED")] });
   
     const reason = args.slice(1).join(" ") || 'No Reason'
