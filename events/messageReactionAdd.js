3401169813232682d4e303d6351358c728faeeda
Neeonn <srdjanneon@gmail.com>
2021-12-02 22:47:33 +0100

    starboard format

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -30,10 +30,9 @@ module.exports = async(client, reaction, user) => {
         board.embeds[0].footer.text = `${count} ${starboard.StarBoard_Emoji}`
         board.edit({ embeds: [board.embeds[0]] })
       } else if (reaction.count >= starboard.Minimum_Reactions) {
-        const embed = client.embedBuilder(client, message, reaction.message.author.username, "")
-          .setThumbnail(reaction.message.author.displayAvatarURL({ dynamic: true }))
+        const embed = client.embedBuilder(client, message, "⭐︲" + reaction.message.author.username, "#ffc87e")
 
-        if (reaction.message.content) embed.setDescription(`> ${reaction.message.content}\n\n[Click Here to View Message](${reaction.message.url})`)
+        if (reaction.message.content) embed.setDescription(`> ${reaction.message.content}\n\n[<:ArrowRightGray:813815804768026705> Click Here to View Message](${reaction.message.url})`)
         if (['png', 'jpg', 'jpeg', 'gif', 'webp'].some(e => (reaction.message.attachments.first() || { url: '' }).url.endsWith(e))) embed.setImage(reaction.message.attachments.first().url)
         let msg = await schannel.send({ embeds: [embed] })
         db.set(`stars_${reaction.message.guild.id}_${reaction.message.id}`, msg.id);
