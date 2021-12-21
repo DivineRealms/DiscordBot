08377ab6331f4968ad468a7f1c4318f6394ce8e0
Neeonn <srdjanneon@gmail.com>
2021-12-21 01:34:56 +0100

    embedColor

--- utils/embedBuilder.js
+++ utils/embedBuilder.js
@@ -6,7 +6,7 @@ module.exports = (client, user, author, description, color) => {
   if (author.length > 0) embed.setAuthor(author);
   if (description.length > 0) embed.setDescription(description);
   if (color) embed.setColor(color);
-  else embed.setColor(client.conf.Settings.embedColor);
+  else embed.setColor(client.conf.Settings.Embed_Color);
 
   return embed;
 };
