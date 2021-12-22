58b01e173e8c58f2518c8aab2b5c8d568008d4b9
niikoolaa04 <nikolajovicic.business@gmail.com>
2021-12-22 17:05:30 +0100

    Fix omgg

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -3,6 +3,55 @@ const db = require("quick.db");
 module.exports = async (client, oldMember, newMember) => {
   const settings = client.conf;
 
+  // Join
+  if (
+    settings.welcomeSystem.welcomeType === "embed" &&
+    [...newMember.roles.cache.keys()].join("") !==
+      [...oldMember.roles.cache.keys()].join("")
+  ) {
+    const addedRoles = newMember.roles.cache.filter(
+      (r) => !oldMember.roles.cache.has(r.id)
+    );
+
+    const added = addedRoles.map((r) => r.id);
+    const welcomeChannel = client.channels.cache.get(
+      settings.welcomeSystem.welcomeChannel
+    );
+
+    if (added.includes("597888019663421440")) {
+      const embed = client
+        .embedBuilder(
+          client,
+          "",
+          "",
+          settings.welcomeSystem.welcomeEmbed.description.replace(
+            "{member}",
+            newMember.user.toString()
+          ),
+          settings.welcomeSystem.welcomeEmbed.color
+        )
+        .setAuthor(
+          settings.welcomeSystem.welcomeEmbed.title
+            .replace("{username}", newMember.user.username)
+            .replace("{joinPosition}", `${newMember.guild.memberCount}`),
+          `https://cdn.upload.systems/uploads/hhgfsHXT.png`
+        )
+        .setThumbnail(
+          newMember.displayAvatarURL({ size: 1024, dynamic: true })
+        );
+
+      if (welcomeChannel)
+        welcomeChannel.send({ embeds: [embed] }).then((msg) =>
+          db.set(`wlcmEmbed_${newMember.guild.id}_${newMember.id}`, {
+            msg: msg.id,
+            channel: msg.channel.id,
+          })
+        );
+    }
+  }
+
+
+  // Leave
   if (
     settings.Welcome_System.Enabled &&
     settings.Welcome_System.Type === "embed" &&
