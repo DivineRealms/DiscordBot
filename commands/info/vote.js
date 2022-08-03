50e61ccf481139620d093908c3cb1485f7cbc8b9
Neeonn <srdjanneon@gmail.com>
2022-08-03 18:01:50 +0200

    Vote

--- commands/info/vote.js
+++ commands/info/vote.js
@@ -9,32 +9,36 @@ module.exports = {
   slash: true,
 };
 
-module.exports.run = async (client, message, args) =>
-  message.channel.send({
-    embeds: [
-      client
-        .embedBuilder(
-          client,
-          message,
-          "Vote Links",
-          `<:DR:765260828714467418> (https://minecraft-mp.com/server/295045/vote/)[**Divine Realms**]
-<:hog:916427016071442442> (https://minecraft-mp.com/server/296478/vote/)[**HogRealms**]`
-        )
-        .setThumbnail(`https://cdn.upload.systems/uploads/YrOfFxGC.png`),
-    ],
-  });
+module.exports.run = async (client, message, args) => {
+  const drButton = new ButtonBuilder()
+    .setEmoji("<:DR:765260828714467418>")
+    .setURL("https://minecraft-mp.com/server/295045/vote/")
+    .setLabel("Vote for Divine Realms")
+    .setStyle(ButtonStyle.Link);
+  const hogButton = new ButtonBuilder()
+    .setEmoji("<:hog:916427016071442442>")
+    .setURL("https://minecraft-mp.com/server/296478/vote/")
+    .setLabel("Vote for HogRealms")
+    .setStyle(ButtonStyle.Link);
 
-module.exports.slashRun = async (client, interaction) =>
-  interaction.reply({
-    embeds: [
-      client
-        .embedBuilder(
-          client,
-          message,
-          "Vote Links",
-          `<:DR:765260828714467418> (https://minecraft-mp.com/server/295045/vote/)[**Divine Realms**]
-<:hog:916427016071442442> (https://minecraft-mp.com/server/296478/vote/)[**HogRealms**]`
-        )
-        .setThumbnail(`https://cdn.upload.systems/uploads/YrOfFxGC.png`),
-    ],
-  });
+  let row = new ActionRowBuilder().addComponents([drButton, hogButton]);
+
+  message.reply({ components: [row] });
+}
+
+module.exports.slashRun = async (client, interaction) => {
+  const drButton = new ButtonBuilder()
+    .setEmoji("<:DR:765260828714467418>")
+    .setURL("https://minecraft-mp.com/server/295045/vote/")
+    .setLabel("Vote for Divine Realms")
+    .setStyle(ButtonStyle.Link);
+  const hogButton = new ButtonBuilder()
+    .setEmoji("<:hog:916427016071442442>")
+    .setURL("https://minecraft-mp.com/server/296478/vote/")
+    .setLabel("Vote for HogRealms")
+    .setStyle(ButtonStyle.Link);
+
+  let row = new ActionRowBuilder().addComponents([drButton, hogButton]);
+
+  interaction.reply({ components: [row] });
+};
