53c82eb6e8021c5538ace28ed4d1bf74038acd8a
Neeonn <srdjanneon@gmail.com>
2023-05-23 14:03:47 +0200

    add regular eval cmd

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -22,7 +22,79 @@ module.exports = {
   ],
 };
 
-module.exports.run = async (client, message, args) => {};
+module.exports.run = async (client, message, args) => {
+  if (!client.conf.Settings.Owner_Discord_ID.includes(message.author.id))
+    return message.channel.send({
+      embeds: [
+        client.utils.errorEmbed(
+          client,
+          message,
+          "Only Developers can use this command."
+        ),
+      ],
+    });
+
+  const code = args.join(" ");
+  if (!code)
+    return message.channel.send({
+      embeds: [
+        client.utils.errorEmbed(
+          client,
+          message,
+          "You need to enter code to evaluate."
+        ),
+      ],
+    });
+  try {
+    let evaled = eval(code);
+
+    if (
+      message.content.toLowerCase().includes("client.token") ||
+      message.content.toLowerCase().includes("token") ||
+      message.content.toLowerCase().includes("client.conf.Settings.Token")
+    )
+      return;
+    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+
+    let embed = client
+      .embedBuilder(client, message, "", "", "Green")
+      .setAuthor({
+        name: "Code Evaluation",
+        iconURL: `https://cdn.upload.systems/uploads/GVd0PBIt.png`,
+      })
+      .addFields({ name: "📥︲Input:", value: `\`\`\`${code}\`\`\`` });
+
+    if (evaled.length >= 1024) {
+      const body = {
+        key: client.conf.Settings.Paste_Key,
+        body: evaled,
+      };
+
+      const response = await fetch("https://api.upload.systems/pastes", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      }).catch((err) => console.log(err));
+
+      const json = await response.json();
+
+      embed.addFields(
+        "📤︲Output:",
+        `\`\`\`xl\nhttps://api.upload.systems/pastes/${json.paste.id}/raw\`\`\``
+      );
+    } else embed.addFields({ name: "📤︲Output", value: `\`\`\`xl\n${evaled}\`\`\`` });
+
+    message.channel.send({ embeds: [embed] });
+  } catch (err) {
+    message.channel.send({
+      embeds: [
+        client.utils
+          .errorEmbed(client, message, "Code Evaluation Failed")
+          .addFields([{ name: "📥︲Input:", value: `\`\`\`xl\n${code}\`\`\`` }, { name: "📤︲Output:", value: `\`\`\`xl\n${err}\`\`\`` }])
+      ],
+    });
+  }
+};
 
 module.exports.slashRun = async (client, interaction) => {
   if (!client.conf.Settings.Owner_Discord_ID.includes(interaction.user.id))
