9c2eb50cee679eeb4b42ca2b01cf8393b37f2394
Neeonn <srdjanneon@gmail.com>
2024-10-25 15:54:21 +0200

    embedBuilder.js

--- utils/embedBuilder.js
+++ utils/embedBuilder.js
@@ -3,10 +3,7 @@ const Discord = require("discord.js");
 module.exports = (client, user, title, description, color) => {
   let embed = new Discord.EmbedBuilder();
 
-  if (title.length > 0) embed.setAuthor({
-    name: title, 
-    iconURL: `https://i.imgur.com/STsjch6.png`,
-  });
+  if (title.length > 0) embed.setAuthor({ name: title });
   if (description.length > 0) embed.setDescription(description);
   if (color) embed.setColor(color);
   else embed.setColor(client.conf.Settings.Embed_Color);
